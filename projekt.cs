835b65c392e60691e099488aa5ccc32ad813a5e2
Tatiana Yakovleva <mellyorchid@gmail.com>
2022-12-13 13:13:45 +0300

    итоговые оформление и удаление проверочных строчек

--- projekt.cs
+++ projekt.cs
@@ -1,5 +1,4 @@
-﻿//
-/*
+﻿/*
     Написать программу, которая из имеющегося массива строк формирует
     массив из строк, длина которых меньше либо равна трём символам.
     Первоначальный массив можно ввести с клавиватуры, либо
@@ -8,34 +7,29 @@
     исключиьельно массивами.
 */
 
-
-/* Формирование массива */
-
+/* функция заполнения массива */
 string Get_Array(string a){
     a = Console.ReadLine();
     return a;
 }
 
+/* объявление переменных и массива*/
 int size = int.Parse(Console.ReadLine());
 int count = 0;
 int size2 = 0;
-
 string[] arr = new string[size];
 
+/* вызов функции для заполнения массива и поиск количества удовлетворяющих условию элементов */
 for (int i = 0; i < size; i++){
     arr[i] = Get_Array(arr[i]);
-}
-
-for (int i = 0; i < size; i++){
     if (arr[i].Length <= 3){
         count++;
     }
 }
 
-Console.WriteLine(count);
-
 string[] farr = new string[count];
 
+/* функция заполняющая второй массив, в котором содержаться только удовлетворяющие условию элементы */
 string[] Fun_arr(string[] mas){
     for (int i = 0; i < mas.Length; i++){
         if (mas[i].Length <= 3){
@@ -53,5 +47,4 @@ if (count == 0){
     for (int i = 0; i < farr.Length; i++){
         Console.Write("{0} ", farr[i]);
     }
-    
 }
